2ebb5466e034a19c55672a94fcf3167bc9c3cec1
Marc McDougall <marc@kilobytestudios.org>
2018-03-26 14:32:52 -0400

    Added initial Google maps integration.

--- src/dependencies/main.js
+++ src/dependencies/main.js
@@ -12,11 +12,21 @@ var ENV_DEV = true;
 
 var app = new Vue({
   
-  el: '#app',
-  data: {
-	
-	status: 'ready'
-  }
+	el: '#app',
+	data: {
+
+		status: 'ready',
+		locations: []
+	},
+	created: function(){
+
+		console.log('Ready.');
+
+	},
+	methods: {
+
+		prepLocations: prepLocations
+	}
 
 });
 
@@ -33,5 +43,52 @@ var app = new Vue({
 // Helper Functions
 // ==========
 
+// Prepares a list of the nearby brunch locations.
+function prepLocations(){
 
-// Coming soon.
+	// Documentation Here: https://developers.google.com/places/web-service/search?authuser=1
+	var apiKey = 'AIzaSyD5KokvEpZuhbLRTUwUNoLU27LdTVeo8Ho';
+	var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.762480,-84.359116&radius=10000&opennow=true&types=restaurant&keyword=brunch&key=' + apiKey;
+
+	console.log('Sending request to: ' + url);
+
+	this.$http.get(url).then(function(response){
+
+		// Success!
+		var results = filterResults(response.body.results);
+
+		console.log('\nSEARCH RESULTS');
+		console.log('==============');
+
+		results.forEach(function(el){
+
+			console.log(el.name + ': ' + el.rating);
+		});
+
+		console.log('\n==============\nRaw (filtered) data:');
+		console.log(results);
+
+		this.locations = results;
+
+	}, function(response){
+
+		// There was some error, so log it.
+		console.log(response);
+	});
+}
+
+// Filters the results to remove any results that has a rating less than 4.0.
+function filterResults(results){
+
+	var output = [];
+
+	results.forEach(function(el){
+
+		if(el.rating >= 4.0){
+
+			output.push(el);
+		}
+	});
+
+	return output;
+}
